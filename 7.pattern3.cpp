fcce239ee57be5c7016b9ab5c77f378c698df5cf
Pranjal Nehete <pranjalnehete02@gmail.com>
2022-08-26 23:53:14 +0530

    Pattern 3
    Love_Babbar YT

---
+++ 7.pattern3.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int count=1;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<count;
+            count=count+1;
+        }
+        cout<<endl;
+    }
+
+/*
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+        cout<<count;
+        count=count+1;
+        j=j+1;
+                    }
+    cout<<endl;
+    i=i+1;
+    }
+ */   
+    return 0;
+}
