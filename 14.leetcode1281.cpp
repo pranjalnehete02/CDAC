9c31a06d45aed706b1b0b047d208071abd9a9e8d
Pranjal Nehete <pranjalnehete02@gmail.com>
2022-09-02 20:15:34 +0530

    Leetcode_LoveBabbar
    Love_babbar V5

---
+++ 14.leetcode1281.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    int prod=1;
+    int sum=0;
+
+    while(n!=0){
+        int digit = n%10;
+        prod=prod*digit;
+        sum = sum + digit;
+        n= n/10;
+    }
+    cout<<sum<<endl;
+    cout<<prod<<endl;
+    int answer = prod-sum;
+
+    cout<<answer<<endl;
+
+    return 0;
+}
