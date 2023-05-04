bd50a25fa204a551d3e1deb1d9ddaef55f5e7647
Pranjal Nehete <pranjalnehete02@gmail.com>
2023-05-04 21:05:54 +0530

    Array

---
+++ 56.Array.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void Rotate_array(int arr[], int n){
+
+    int last_element = arr[n-1];
+    
+    for(int i=n-2 ; i>=0; i--){
+        arr[i+1] = arr[i];
+    }
+    
+    arr[0] = last_element;
+
+}
+
+int main(){
+
+    int arr[] = {1,2,3,4,5};
+
+    int n = sizeof(arr);
+
+    Rotate_array(arr, n);
+
+    for(int i = 0; i < n; i++){
+
+        cout<<arr[i]<<endl;
+
+    }
+    return 0;
+}
