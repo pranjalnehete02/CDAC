45136155a81e11c9d3cf661e23af782df50b3f1e
Pranjal Nehete <pranjalnehete02@gmail.com>
2023-05-05 23:50:53 +0530

    Kadane's algorithm

---
+++ 59.Array.cpp
@@ -0,0 +1,16 @@
+/* Given an array arr of N elements 
+   we need to find the contiguous sub array with max-sum.
+
+                    KADANE'S ALGORITHM
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    
+
+    return 0;
+}
