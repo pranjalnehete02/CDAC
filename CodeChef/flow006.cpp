03f66cbb0fe57d684935dc2e1b6843b3b00bb220
Pranjal Nehete <pranjalnehete02@gmail.com>
2023-07-23 15:52:17 +0530

    Codechef
    Flow006

---
+++ CodeChef/flow006.cpp
@@ -0,0 +1,37 @@
+
+/*
+
+Problem
+You're given an integer N. Write a program to calculate the sum of all the
+digits of N.
+
+                        TRADITIONAL METHOD 1
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x, sum=0; cin>>x;
+    while(x>0){
+        sum = sum + x%10;
+        x = x/10;
+    }
+    
+    cout<<sum<<endl;
+    return 0;
+}
+
+
+*/
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    while(T--)
+
+    return 0;
+}
