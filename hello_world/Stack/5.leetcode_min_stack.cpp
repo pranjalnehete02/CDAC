1a31e67633c19e4dc242afd508b3e8593d6c4a47
Pranjal Nehete <pranjalnehete02@gmail.com>
2023-08-14 23:58:12 +0530

    Leetcode

---
+++ hello_world/Stack/5.leetcode_min_stack.cpp
@@ -0,0 +1,34 @@
+/*
+
+class min_class{
+    stack<int>st, s2;
+    minStock() {
+
+    }
+
+    void push(int val){
+        if(s2.empty() || val <= s2.top()){
+            s2.push(val);
+        }
+    }
+
+    void pop(){
+
+        if(st.top() == s2.top()){
+            s2.pop();
+        }
+        st.pop(val);
+    }
+
+    void top(){
+        return s2.top();
+    }
+
+    void get_min{
+        return s2.top();
+    }
+
+};
+
+
+*/
