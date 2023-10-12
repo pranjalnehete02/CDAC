8a7e6d6d5957e6238a40948d73174ae41a36aa85
Pranjal Nehete <pranjalnehete02@gmail.com>
2023-10-12 20:04:08 +0530

    Introduction to LinkedList

---
+++ hello_world/LinkedList/Intro.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//linked list node
+
+struct node{
+    int data;
+    node *next;
+};
+
+
+int main(){
+
+    node *head = new node();
+
+    int *ptr = new int;
+
+    int a;
+
+    return 0;
+}
