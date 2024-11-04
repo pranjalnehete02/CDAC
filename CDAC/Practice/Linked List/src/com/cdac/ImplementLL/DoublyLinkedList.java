8be74296a0bec27e9e8e2f938258c12b9ba33457
Pranjal <pranjalnehete02@gmail.com>
2024-11-05 00:59:59 +0530

    Doubly Linked List Add All operations Implementation

--- CDAC/Practice/Linked List/src/com/cdac/ImplementLL/DoublyLinkedList.java
+++ CDAC/Practice/Linked List/src/com/cdac/ImplementLL/DoublyLinkedList.java
@@ -30,4 +30,40 @@ public class DoublyLinkedList {
 			
 		}
 	}
+	
+	//At end
+	public void addAtEnd(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		}else {
+			Node current = head;
+			while(current.next != null) {
+				current = current.next;
+			}
+			current.next = newNode;
+			newNode.prev = current;
+		}
+	}
+	
+	
+	public void addAtPos(int data, int position) {
+		Node newNode = new Node(data);
+		
+		if(head == null || position <= 1) {
+			addAtFront(data);
+		}
+		else {
+			Node current = head;
+			for(int i = 1; (i < position - 1) && (current.next != null); i++ ) {
+				current = current.next;
+			}
+			newNode.next = current.next;
+			if (current.next != null) {
+				current.next.prev = newNode;
+			}
+			newNode.prev = current;
+			current.next = newNode;
+		}
+	}
 }
