aae8bcb378cd66c4eb8a20d9275537ff388f3363
Pranjal <pranjalnehete02@gmail.com>
2024-11-05 00:30:08 +0530

    Doubly Linked List AddFront operations Implementation

--- CDAC/Practice/Linked List/src/com/cdac/ImplementLL/DoublyLinkedList.java
+++ CDAC/Practice/Linked List/src/com/cdac/ImplementLL/DoublyLinkedList.java
@@ -1,5 +1,33 @@
 package com.cdac.ImplementLL;
 
 public class DoublyLinkedList {
+	class Node{
+		int data;
+		Node prev;
+		Node next;
+		
+		public Node(int data) {
+			this.data = data;
+			this.prev = null;
+			this.next = null;
+		}
+	}
 	
+	Node head = null;
+	
+					//ADD
+	public void addAtFront(int data) {
+		Node newNode = new Node(data);
+		//if list is empty
+		if(head == null) {
+			head = newNode;
+		}else {
+			newNode.next = head;
+			head.prev = newNode;
+			head = newNode;
+			
+			
+			
+		}
+	}
 }
