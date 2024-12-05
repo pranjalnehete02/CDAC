ce26022da5294138d2e594e992748371485ea0ed
Pranjal Nehete <pranjalnehete02@gmail.com>
2024-12-05 18:12:37 +0530

    TaskMGR

---
+++ Web Dev/Task Manager/script.js
@@ -0,0 +1,11 @@
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`, // This will provide a unique ID for every task we create
+    imageUrl: document.getElementById("Image_url").value,
+    taskTitle: document.getElementById("Task_title").value,
+    taskType: document.getElementById("Task_type").value,
+    taskDescription: document.getElementById("Task_description").value,
+  };
+
+  console.log(taskData);
+};
