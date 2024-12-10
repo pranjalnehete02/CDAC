55f11ee9838aaf93edeade94007f453f699f48c3
Pranjal Nehete <pranjalnehete02@gmail.com>
2024-12-11 02:35:10 +0530

    NodeJS Server creation

--- Web Dev/NodeJS/HTTP/index.js
+++ Web Dev/NodeJS/HTTP/index.js
@@ -1,8 +1,13 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-  console.log("New reeq received");
-  res.end("hello from server");
+  // console.log("New reeq received");
+  // console.log(req);
+  const log = `${Date.now()}: ${req.url} New Request received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    res.end("hello from server");
+  });
 });
 
 myServer.listen(8800, () => console.log("Sserver started"));
