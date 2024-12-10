fe5127eb1efbe97afdcbe9af87145de4ac2f5157
Pranjal Nehete <pranjalnehete02@gmail.com>
2024-12-10 06:36:15 +0530

    API Weather Display app

--- Web Dev/JS/Interacting with API and etc/script.js
+++ Web Dev/JS/Interacting with API and etc/script.js
@@ -1,5 +1,21 @@
 const button = document.getElementById("search-button");
+const input = document.getElementById("city-input");
 
-button.addEventListener("click", () => {
-  alert("clicked");
+//http://api.weatherapi.com/v1/current.json?key=021386292ced422591c01723241012&q=London&aqi=yes
+
+async function getData(cityName) {
+  const data = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=021386292ced422591c01723241012&q=${cityName}&aqi=yes`
+  );
+
+  //   console.log(await data.json());
+  //   getData();
+  //   console.log(getData);
+  return await data.json();
+}
+
+button.addEventListener("click", async () => {
+  const value = input.value;
+  const result = await getData(value);
+  console.log(result);
 });
