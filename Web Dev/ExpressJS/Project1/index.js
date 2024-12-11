1511731f7857b5692e06350fafb4626284d7695a
Pranjal <pranjalnehete02@gmail.com>
2024-12-12 02:21:20 +0530

    Node Project Piyush Garg 12/12/24

--- Web Dev/ExpressJS/Project1/index.js
+++ Web Dev/ExpressJS/Project1/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
-
+const user = require("./MOCK_DATA.json");
 const app = express();
 
 const port = 8000;
 
 
-//routes
+                                    //routes
+//         1. GET /users - List all the users
+app.get("/api/users", (req, res) => {
+    return res.json(users);
+})
+
+app.get("/users", (req, res) => {
+    const html =`
+    <ul>
+        ${user.map((user) => `<li>${user.first_name}</li>`).join("")}
+    </ul>`;
+    res.send(html);
+    
+});
+
 app.listen(port, () => console.log("Server is running !!"));
