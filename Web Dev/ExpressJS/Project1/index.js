e50679e224bda22b5ee005038ba1123c446b2517
Pranjal <pranjalnehete02@gmail.com>
2024-12-12 02:39:15 +0530

    Node Project Piyush Garg 12/12/24

--- Web Dev/ExpressJS/Project1/index.js
+++ Web Dev/ExpressJS/Project1/index.js
@@ -20,4 +20,20 @@ app.get("/users", (req, res) => {
     
 });
 
+
+// GET /users/1 - Get the user with id 1
+// GET /users/2 - Get the user with id 2
+//Dynamic Path parameters
+//GET/api/users:id   --->dynamic
+
+
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = usersData.find((user) => user.id === id);
+    if (!user) {
+        return res.status(404).json({ message: "User  not found" });
+    }
+    return res.json(user);
+});
+
 app.listen(port, () => console.log("Server is running !!"));
