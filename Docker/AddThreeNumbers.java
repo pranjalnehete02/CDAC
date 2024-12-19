4c42cc235a194af331bc4efea837feb81a9e6a2a
Pranjal <pranjalnehete02@gmail.com>
2024-12-19 23:06:10 +0530

    Docker

---
+++ Docker/AddThreeNumbers.java
@@ -0,0 +1,12 @@
+public class AddThreeNumbers{
+
+public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 20;
+    int num3 = 30;
+
+    int sum = num1 + num2 + num3;
+    System.out.println("Sum of three numbers is: " + sum);
+}
+
+}
