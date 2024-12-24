fb897b2091d40b758e708ad4192ca68a467bf3f6
Pranjal Nehete <pranjalnehete02@gmail.com>
2024-12-25 04:37:08 +0530

    servlet method = POST

---
+++ ADV JAVA/DemoApp/src/main/java/com/cdac/firstDemo/Authntication.java
@@ -0,0 +1,19 @@
+package com.cdac.firstDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Authntication extends HttpServlet{
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {    
+		PrintWriter out = response.getWriter();
+		out.println("Authentication is done !!");
+		
+	}
+	
+	
+}
--- ADV JAVA/DemoApp/src/main/java/com/cdac/firstDemo/LoginAuth.java
+++ ADV JAVA/DemoApp/src/main/java/com/cdac/firstDemo/LoginAuth.java
@@ -1,8 +1,9 @@
 package com.cdac.firstDemo;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,15 +15,18 @@ public class LoginAuth extends HttpServlet{
 	 */
 	private static final long serialVersionUID = 1L;
 
-		public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 			String user_name = request.getParameter("user_name");
 			String user_pw = request.getParameter("user_pw");
 			
 //			System.out.println(user_name);
 //			System.out.println(user_pw);
 			
-			PrintWriter out = response.getWriter();
-			out.println("The user ID is : " + user_name + "The pw is 	: " + user_pw);
+//			PrintWriter out = response.getWriter();
+//			out.println("The user ID is : " + user_name + "The pw is 	: " + user_pw);
+//			
 			
+			RequestDispatcher ReqDisp = request.getRequestDispatcher("Auth");
+			ReqDisp.forward(request, response);
 		}
 }
