851e1cd8f1227114e1493f9263c3415c115fbe0e
Pranjal Nehete <pranjalnehete02@gmail.com>
2024-12-24 23:11:44 +0530

    servlet creation LoginAuth proj

---
+++ ADV JAVA/DemoApp/src/main/java/com/cdac/firstDemo/LoginAuth.java
@@ -0,0 +1,28 @@
+package com.cdac.firstDemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginAuth extends HttpServlet{
+	
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+		public void Service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+			String user_name = request.getParameter("user_name");
+			String user_pw = request.getParameter("user_pw");
+			
+//			System.out.println(user_name);
+//			System.out.println(user_pw);
+			
+			PrintWriter out = response.getWriter();
+			out.println("The user ID is : " + user_name + "The pw is 	: " + user_pw);
+			
+		}
+}
